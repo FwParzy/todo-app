e94ac89718dd022d945b67e9398b02f00da6fe38
FwParzy <guardianiisaac@gmail.com>
2023-09-07 13:23:11 -0400

    Remove dist folder from repository

--- dist/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const { contextBridge } = require('electron');
-const { v4: uuidv4 } = require('uuid');
-contextBridge.exposeInMainWorld('api', {
-    generateUUID: () => uuidv4(),
-});
