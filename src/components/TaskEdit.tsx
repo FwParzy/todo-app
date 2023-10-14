a8c8ffec4e13f30370e7d747051af74dcc8bd4dc
FwParzy <guardianiisaac@gmail.com>
2023-10-14 15:44:17 -0400

    Added Delete button

--- src/components/TaskEdit.tsx
+++ src/components/TaskEdit.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { handleEnterKey } from '../utils/keyboardUtils';
 
 interface Props {
@@ -8,18 +9,30 @@ interface Props {
 
 const TaskEdit = ({ inputRef, onCancel, onOk }: Props) => {
 
+  useEffect(() => {
+    const openTasks = document.querySelectorAll('.task_input');
+    if (openTasks.length === 2) {
+      const task1Name = (openTasks[0] as HTMLInputElement)?.value;
+      const task2Name = (openTasks[1] as HTMLInputElement)?.value;
+
+      console.log(`Doing things to ${task1Name} and ${task2Name}`);
+    }
+  }, []);
+
   return (
-    <div>
+    <div className='task_edit'>
       <input
         ref={inputRef}
         type="text"
         name="inputCategory"
         placeholder="Enter Task"
         onKeyDown={(e) => handleEnterKey(e, onOk)}
+        className='task_input'
         autoFocus
       />
       <button onClick={onCancel}>Cancel</button>
-      <button onClick={onOk}>Ok</button>
+      <button onClick={onOk} className='task_ok'>Ok</button>
+      <button className='task_delete'>Delete</button>
     </div>
   )
 }
