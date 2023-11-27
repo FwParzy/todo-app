e4ba7ae7b969c8f2870ad1dac44a6fa3ee67fce9
FwParzy <guardianiisaac@gmail.com>
2023-11-27 16:55:43 -0500

    reorder_tasks_init

--- server/seeders/dbSeeder.js
+++ server/seeders/dbSeeder.js
@@ -39,6 +39,7 @@ rl.question('Enter MySQL username: ', (username) => {
     id INT AUTO_INCREMENT PRIMARY KEY,
     userId INT NOT NULL,
     categoryId INT NOT NULL,
+    order INT NULL,
     name VARCHAR(255) NOT NULL,
     completed BOOLEAN NOT NULL DEFAULT FALSE,
     createTs TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
